ea915c7f0042802bdfc914e5bd165d07cadbeb0f
Haron_Richer <ertui03@mail.ru>
2025-03-14 16:03:28 +0700

    finish 1.4

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -6,7 +6,6 @@ import db.Service.ServiceHibernate;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import db.Models.User;
 import org.springframework.web.bind.annotation.PostMapping;
